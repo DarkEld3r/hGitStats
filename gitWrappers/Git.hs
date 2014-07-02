beb674aa6d6f221a09a03fc975c16dea866708af
DarkEld3r <stanislav.tkach@gmail.com>
2014-07-03 00:41:24 +0300

    checkResult

--- gitWrappers/Git.hs
+++ gitWrappers/Git.hs
@@ -9,26 +9,23 @@ import Foreign.C
 
 import GitWrappers
 
--- TODO: 'Varargs'
---checkError :: String -> (IO() -> IO CInt) -> IO ()
---checkError errorMessage a = do
---  result <- a
---  when (0 /= result) $ 
---    error $ errorMessage ++ " Error code = '" ++ (show result) ++ "'."
---  return ()
+-- Reports error if result is not zero.
+checkResult :: IO CInt -> String -> IO ()
+checkResult ioResult errorMessage = do
+  result <- ioResult
+  when (0 /= result) $ 
+    error $ errorMessage ++ " Error code = '" ++ (show result) ++ "'."  
+  return ()
 
 gitRepositoryOpen :: String -> IO GitRepository
 gitRepositoryOpen path = do
   let repository = nullPtr
-  result <- withCString path $ \path' -> git_repository_open repository path'
-  when (0 /= result) $ 
-    error $ "Unable to open '" ++ path ++ "' repository. Error = '" ++ (show result) ++ "'."
+  checkResult (withCString path $ \path' -> git_repository_open repository path') 
+    $ "Unable to open '" ++ path ++ "' repository."
   return repository
 
 gitRevwalkNew :: GitRepository -> IO GitRevwalk
 gitRevwalkNew repository = do
   let revwalk = nullPtr
-  result <- git_revwalk_new revwalk repository 
-  when (0 /= result) $ 
-    error $ "Unable to create revision walker. Error = '" ++ (show result) ++ "'."
+  checkResult (git_revwalk_new revwalk repository) "Unable to create revision walker."
   return revwalk
