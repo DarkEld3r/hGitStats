802bf35277a5eb066c48c44f95c3e6a305b82ba3
DarkEld3r <stanislav.tkach@gmail.com>
2014-07-10 01:35:01 +0300

    gitReferenceNameToId

--- gitWrappers/GitOid.hs
+++ gitWrappers/GitOid.hs
@@ -25,3 +25,13 @@ gitOidFromStr hash = do
   checkResult (withCString hash $ \hash' -> git_oid_fromstr oid hash') 
     $ "git_oid_fromstr(" ++ hash ++ ") failed."
   return oid
+
+foreign import ccall git_reference_name_to_id :: GitOid -> GitRepository -> CString -> IO CInt
+
+gitReferenceNameToId :: GitRepository -> String -> IO GitOid
+gitReferenceNameToId repository name = do
+  -- TODO: FIXME: alloca?
+  let oid = nullPtr
+  checkResult (withCString name $ \name' -> git_reference_name_to_id oid repository name') 
+    $ "git_reference_name_to_id(" ++ name ++ ") failed."
+  return oid
