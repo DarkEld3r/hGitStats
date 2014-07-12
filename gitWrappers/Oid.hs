d8cd30a21823753b4d1690400b02b69de09284c3
DarkEld3r <stanislav.tkach@gmail.com>
2014-07-13 02:14:54 +0300

    createOid/freeOid

--- gitWrappers/Oid.hs
+++ gitWrappers/Oid.hs
@@ -3,6 +3,8 @@
 
 module Oid
   ( Oid
+  , createOid
+  , freeOid
   , oidFromStr
   , referenceNameToId
   , headId
@@ -11,55 +13,43 @@ module Oid
 import Foreign.C.Types
 import Foreign.C.String
 import Foreign.Ptr
-
--- TODO: FIXME
 import Foreign.Marshal.Alloc
-import Foreign.Storable
 
 import Common
 import Repository
 
 -- git_oid
--- TODO: Array (fixed size)?
-oidRawSize = 20
-data CGitOid = CGitOid { id :: CString }
+data CGitOid
 type Oid = Ptr CGitOid
 
-instance Storable CGitOid where
-  alignment _ = (undefined :: Int)
-  sizeOf _ = oidRawSize
-  peek _ = error "peek is not implemented"
-  poke ptr (CGitOid id') = do
-    withCStringLen (take oidRawSize value) $ uncurry (copyArray $ #{ptrc_type, c_string_field} ptr)
---    (#poke ArrayStruct, a) ptr a'
---    (#poke ArrayStruct, b) ptr b'
---    (#poke ArrayStruct, c) ptr c'
+oidSize :: Int
+oidSize = 20
+
+createOid :: IO Oid
+createOid = do
+  mallocBytes oidSize
+
+freeOid :: Oid -> IO ()
+freeOid oid = do
+  free oid
 
 foreign import ccall git_oid_fromstr :: Oid -> CString -> IO CInt
 
 oidFromStr :: String -> IO Oid
 oidFromStr hash = do
-  -- TODO: FIXME: alloca?
-  let oid = nullPtr
+  oid <- createOid
   checkResult (withCString hash $ \hash' -> git_oid_fromstr oid hash') 
     $ "git_oid_fromstr(" ++ hash ++ ") failed."
   return oid
 
 foreign import ccall git_reference_name_to_id :: Oid -> Repository -> CString -> IO CInt
 
---referenceNameToId :: Repository -> String -> IO Oid
---referenceNameToId repository name = do
---  let oid = nullPtr
---  checkResult (withCString name $ \name' -> git_reference_name_to_id oid repository name') 
---    $ "git_reference_name_to_id(" ++ name ++ ") failed."
---  return oid
-
 referenceNameToId :: Repository -> String -> IO Oid
-referenceNameToId repository name = alloca $ \oid -> do
+referenceNameToId repository name = do
+  oid <- createOid
   checkResult (withCString name $ \name' -> git_reference_name_to_id oid repository name') 
     $ "git_reference_name_to_id(" ++ name ++ ") failed."
   return oid
-
 
 headId :: Repository -> IO Oid
 headId repository = referenceNameToId repository "HEAD"
