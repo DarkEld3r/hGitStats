b14433749d0ec660e40a85a95551ea480533b60b
DarkEld3r <stanislav.tkach@gmail.com>
2014-07-17 08:59:18 +0300

    TODO

--- hGitStats/Main.hs
+++ hGitStats/Main.hs
@@ -21,5 +21,33 @@ main = do
   repository <- repositoryOpen path
   headOid <- headId repository
   freeOid headOid
+
+-- TODO: FIXME:
+{-|
+    git_revwalk_new(&walker, repo);
+    git_revwalk_sorting(walker, GIT_SORT_TOPOLOGICAL);
+    git_revwalk_push(walker, &oid);
+
+    const char *commit_message;
+    const git_signature *commit_author;
+
+    while(git_revwalk_next(&oid, walker) == GIT_SUCCESS) {
+        if(git_commit_lookup(&commit, repo, &oid)){
+            fprintf(stderr, "Failed to lookup the next object\n");
+            return 1;
+        }
+
+        commit_message  = git_commit_message(commit);
+        commit_author = git_commit_committer(commit);
+
+        // Don't print the \n in the commit_message 
+        printf("'%.*s' by %s <%s>\n", strlen(commit_message)-1, commit_message, commit_author->name, commit_author->email);
+
+        git_commit_free(commit);
+    }
+
+    git_revwalk_free(walker);
+-}
+
   repositoryFree repository
   return ()
